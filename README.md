3beac950ada13e5d757a995b2da96da452172a44
hendisantika <hendisantika@yahoo.co.id>
2019-03-28 23:16:43 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-protobuf-example
---
+++ src/main/java/com/hendisantika/springbootprotobufexample/SpringbootProtobufExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootprotobufexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProtobufExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProtobufExampleApplication.class, args);
+    }
+
+}
